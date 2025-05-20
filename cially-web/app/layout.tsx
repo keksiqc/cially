91fb037ee8790b15a90da7787a3dd596f726eef3
skellgreco <tixisb@gmail.com>
2025-05-21 01:34:32 +0300

    Added color theme logic on layout.tsx (global layout)

--- cially-web/app/layout.tsx
+++ cially-web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./_components/_shadcn/theme-provider";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
 	title: "Cially Dashboard",
@@ -16,14 +17,25 @@ export const viewport: Viewport = {
 	userScalable: false,
 };
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
+	const cookieStore = await cookies();
+	const theme = cookieStore.get("theme") || {value: "blue"};
+	console.log(theme);
+	const gridClass = {
+			blue: "bg-gr",
+			gray: "bg-gr-gray",
+			pink: "bg-gr-pink",
+			brown: "bg-gr-brown",
+		}[theme.value];
+	console.log(gridClass)
+
 	return (
 		<>
 			<html lang="en" suppressHydrationWarning>
 				<head />
 				<body className="">
 					<div className="overflow-x-hidden min-h-screen">
-						<div className="bg-gr fixed inset-0 w-full h-full -z-10"></div>
+						<div className={`${gridClass} fixed inset-0 w-full h-full -z-10`} />
 						<div className="relative z-0 p-6">
 							<ThemeProvider
 								attribute="class"
@@ -31,8 +43,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 								enableSystem
 								disableTransitionOnChange
 							>
-									{children}
-
+								{children}
 							</ThemeProvider>
 						</div>
 					</div>
